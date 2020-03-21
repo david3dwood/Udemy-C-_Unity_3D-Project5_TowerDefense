fe8d7897baf7e77649a36f2b8222b8867b62fffd
real3ddave <david3dwood@gmail.com>
2020-03-21 11:51:13 -0400

    initial project commit
    INITIAL FILES PUSH

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+[RequireComponent (typeof (Waypoint))]
+public class CubeEditor : MonoBehaviour {
+    // [SerializeField][Range (1f, 20f)] float gridSize = 10f;
+
+    Waypoint waypoint;
+
+    private void Awake () {
+        waypoint = GetComponent<Waypoint> ();
+    }
+
+    void Update () {
+        SnapToGrid ();
+        UpdateLabel ();
+    }
+
+    private void SnapToGrid () {
+        int gridSize = waypoint.GetGridSize ();
+        Vector3 gridPos3D = transform.position = new Vector3 (waypoint.GetGridPos ().x, 0f, waypoint.GetGridPos ().y);
+    }
+
+    private void UpdateLabel () {
+        TextMesh textMesh = GetComponentInChildren<TextMesh> ();
+        int gridSize = waypoint.GetGridSize ();
+        string labelText = waypoint.GetGridPos ().x / gridSize + "," + waypoint.GetGridPos ().y / gridSize;
+
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] List<Waypoint> path;
+
+    // Start is called before the first frame update
+    void Start () {
+        StartCoroutine (FollowPath ());
+        print("back at start");
+    }
+
+    IEnumerator FollowPath () {
+        print ("Starting patrol...  ");
+        foreach (Waypoint waypoint in path) {
+            print ("visiting waypoint  " + (waypoint.name));
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds (1f);
+        }
+        print ("Ending patrol");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
